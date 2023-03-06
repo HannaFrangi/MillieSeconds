d10c35241053be9a6387e73cd37fa650a382cb35
HannaFrangi <43037389+HannaFrangi@users.noreply.github.com>
2023-03-06 16:34:51 +0200

    Update
    Updated

--- src/millieseconds/MillieSeconds.java
+++ src/millieseconds/MillieSeconds.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package millieseconds;
-
+import java.util.Scanner;
 /**
  *
  * @author USER
@@ -15,6 +15,20 @@ public class MillieSeconds {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number of milliseconds: ");
+        long milliseconds = scanner.nextLong();
+
+        long hours = milliseconds / 3600000;
+        milliseconds = milliseconds % 3600000;
+
+        long minutes = milliseconds / 60000;
+        milliseconds = milliseconds % 60000;
+
+        long seconds = milliseconds / 1000;
+
+        System.out.printf("%d hours, %d minutes, %d seconds", hours, minutes, seconds);
     }
     
 }
